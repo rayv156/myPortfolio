69c2040123c912faa07bdd788a9f8fbdedddbaa9
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-12-14 20:57:18 -0600

    fixed images

--- components/header.js
+++ components/header.js
@@ -11,21 +11,21 @@ const Header = () => {
             <nav style={navbarStyle}>
            <div className="nav-wrapper" >
            <ul id="nav-mobile" className="center hide-on-med-and-down">
-           <li><a href="/" style={{textDecoration: 'none', color: 'white'}}><div className="underline" >Skills</div></a></li>
-                <li><a className="navbar-brand" href="/resume" style={{color: 'white'}}><div className="underline" >Resume</div></a></li>
+           <li><a href="#skills" style={{textDecoration: 'none', color: 'white', fontSize: 20}}><div className="underline" >Skills</div></a></li>
+                <li><a className="navbar-brand" href="/resume" style={{color: 'white', fontSize: 20}}><div className="underline" >Resume</div></a></li>
                 </ul>
                     <a href="/" className="brand-logo center" style={{textDecoration: 'none', color: 'white'}}>Ray Velasquez</a>
                     <a href="#" data-target="mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                
-                    <li><a href="/projects" style={{textDecoration: 'none', color: 'white'}}><div className="underline" >Projects</div></a></li>
-                    <li><a href="/blog" style={{textDecoration: 'none', color: 'white'}}><div className="underline">Blog</div></a></li>
+                    <li><a href="/projects" style={{textDecoration: 'none', color: 'white', fontSize: 20}}><div className="underline" >Projects</div></a></li>
+                    <li><a href="/blog" style={{textDecoration: 'none', color: 'white', fontSize: 20}}><div className="underline">Blog</div></a></li>
                 </ul>
             </div>
             </nav>
 
-            <ul className="sidenav" id="mobile">
-                <li><a href="/" style={{textDecoration: 'none', color: 'black'}}><div >Skills</div></a></li>
+            <ul className="sidenav" id="mobile" style={{backgroundImage: 'url("https://i.imgur.com/25FpjT4.png")', backgroundPosition: 'center'}}>
+                <li><a href="#skills" style={{textDecoration: 'none', color: 'black'}}><div >Skills</div></a></li>
                 <li><a className="navbar-brand" href="/resume" style={{color: 'black'}}><div >Resume</div></a></li>
                     <li><a href="/projects" style={{textDecoration: 'none', color: 'black'}}><div >Projects</div></a></li>
                     <li><a href="/blog" style={{textDecoration: 'none', color: 'black'}}><div>Blog</div></a></li>
--- pages/index.js
+++ pages/index.js
@@ -43,19 +43,19 @@ const Index = () => {
       </p>
       </div>
 
-      <h3 style={{backgroundColor: '#EEFBFB'}}>Skills</h3>
+      <h3 id="skills" style={{backgroundColor: '#EEFBFB'}}>Skills</h3>
       <div style={{width: '100%', display: 'flex', justifyContent: 'center', margin: 'auto', flexWrap: 'wrap', backgroundColor: '#EEFBFB', paddingBottom: 70}}>
         <img style={imgStyle} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"/>
         <img style={imgStyle} src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"/>
         <img style={imgStyle} src="https://pbs.twimg.com/media/CZGHPChUAAA3jqE.png"/>
         <img style={imgStyle} src="https://miro.medium.com/max/500/1*cPh7ujRIfcHAy4kW2ADGOw.png"/>
         <img style={imgStyle} src="https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png"/>
-        <img style={imgStyle} src="https://www.brandeps.com/logo-download/N/Node-JS-logo-vector-01.svg"/>
+        <img style={imgStyle} src="https://ih1.redbubble.net/image.1305874484.9819/flat,750x1000,075,f.jpg"/>
         <img style={imgStyle} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTerMgC8YiCGH7WzGhMCQxU_Fn5hFWedV8coQ&usqp=CAU"/>
         <img style={imgStyle} src="https://ucarecdn.com/f49e8fc4-876f-49ef-934f-89812fc4125e/"/>
         <img style={imgStyle} src="https://www.w3.org/html/logo/downloads/HTML5_1Color_Black.png"/>
         <img style={imgStyle} src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/220px-Bootstrap_logo.svg.png"/>
-        <img style={imgStyle} src="https://www.brandeps.com/logo-download/M/Materialize-CSS-logo-vector-01.svg"/>
+        <img style={imgStyle} src="https://colinstodd.com/images/posts/matcss-min.png"/>
         <img style={imgStyle} src="https://d2m06gn7cjwia2.cloudfront.net/express.png"/>
         <img style={imgStyle} src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/141_Git_logo_logos-512.png"/>
         <img style={imgStyle} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"/>
